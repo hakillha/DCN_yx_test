1874874cda8c8cc309afc556258801a74ee1dfcd
hakillha <420116830@qq.com>
2019-12-03 12:01:04 +0000

    Update

--- test.py
+++ test.py
@@ -19,13 +19,13 @@ parser.add_argument('--save_img')
 parser.add_argument('--display_img', help='Also set this to true if want to save output images.')
 args = parser.parse_args()
 
-# args.img_folder_path = '/media/yingges/Data/201910/FT/FTData/ft_det_cleanedup/ignore_toosmall/11_30/images'
+args.img_folder_path = '/home/yingges/data/ft_det/cleanedup_ignoresmall_valid/images'
 # args.valid_json_file = '/media/yingges/Data/201910/FT/FTData/ft_det_cleanedup/ignore_toosmall/11_30/valid.json'
-# args.output_path = output_path = '/media/yingges/Data/201910/FT/FTData/ft_det_cleanedup/ignore_toosmall/11_30/augmented_output.json'
-# args.lib_path = "/media/yingges/Data/201910/Deploy/DCN_GPU_cu100/DCN"
+args.output_path = '/home/yingges/data/ft_det/cleanedup_ignoresmall_valid/img_output'
+args.lib_path = "/home/yingges/experiment/DCN/DCN_yx_test"
 
 img_info = None
-if arg.valid_json_file is not None:
+if args.valid_json_file is not None:
 	gt_json = json.load(open(gt_file))
 	img_info = gt_json['images']
 
@@ -33,8 +33,10 @@ test_img_path = [pj(args.img_folder_path, file) for file in os.listdir(args.img_
 sys.path.append(os.path.abspath(args.lib_path))
 import DCN.fpn.inference as inference
 
+model_path = 'DCN/model/rfcn_dcn_voc'
+
 inference.dataset_img_infer(test_img_path, model_path,
-							output_path,
+							args.output_path,
 							img_info, 'fpn', True, 
 							args.display_img, args.save_img,
 							32)
